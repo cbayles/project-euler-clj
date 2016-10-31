d60226e5580c3ebdbecc85e826ebb086cb018155
Chris Bayles <cbayles81@gmail.com>
2016-10-31 16:58:22 -0500

    problem 2 solved

--- test/project_euler_clj/core_test.clj
+++ test/project_euler_clj/core_test.clj
@@ -10,3 +10,19 @@
   (testing "Sum of multiples below 1000"
     (is (= 233168 (problem01 1000)))))
 
+(deftest problem02-fibonacci-numbers
+  (testing "Find fibonacci numbers below a maximum number"
+    (is (= [0] (fibonacci 0)))
+    (is (= [0] (fibonacci 1)))
+    (is (= [0 1 1] (fibonacci 2)))
+    (is (= [0 1 1 2] (fibonacci 3)))
+    (is (= [0 1 1 2 3 5 8] (fibonacci 10)))
+    (is (= [0 1 1 2 3 5 8 13 21 34 55 89] (fibonacci 100)))))
+
+(deftest problem02-sample-test
+  (testing "Sum of even fibonacci up to 100"
+    (is (= 44 (problem02 100)))))
+
+(deftest problem02-final-test
+  (testing "Sum of even fibonacci up to four million"
+    (is (= 4613732 (problem02 4000000)))))
